f6f3c6871a7cc2a901c241a37eda328b7aa45ac8
grevorski <74430404+grevorski@users.noreply.github.com>
2021-03-12 23:44:57 +0100

    stage 3/5

--- HyperSkill-MineSweeper/Main.java
+++ HyperSkill-MineSweeper/Main.java
@@ -10,8 +10,10 @@ public class Main {
     public static void main(String[] args) {
         fillGrid();
         fillWithMines(grid);
+       // printGrid();
+        fillWithHints(grid);
+        //System.out.println("----------------------------------");
         printGrid();
-
 
     }
 
@@ -24,16 +26,16 @@ public class Main {
     }
 
     public static void printGrid(){
-//        for(int i = 0; i<9; i++)
-//        {
-//            for(int j = 0; j<9; j++)
-//            {
-//                System.out.print(grid[i][j]);
-//            }
-//            System.out.println();
-//        }
-                System.out.println(Arrays.deepToString(grid).replace("], ", "\n").replace("[[", "").replace("]]", "")
-                .replace("[","").replace(", ",""));
+        for(int i = 0; i<9; i++)
+        {
+            for(int j = 0; j<9; j++)
+            {
+                System.out.print(grid[i][j]);
+            }
+            System.out.println();
+        }
+      //  System.out.println(Arrays.deepToString(grid).replace("], ", "\n").replace("[[", "")
+         //               .replace("]]", "").replace("[","").replace(", ",""));
     }
 
     public static void fillWithMines(char[][] grid){
@@ -47,6 +49,95 @@ public class Main {
                 if(grid[randomX][randomY] == '.'){
                     grid[randomX][randomY] = 'X';
                     break;
+                }
+            }
+        }
+    }
+
+    public static void fillWithHints(char[][] grid){
+
+        for (int i = 0; i < 9; i++) {
+
+            for (int j = 0; j < 9; j++) {
+                int temp = 48;
+
+                if(grid[i][j] == '.'){
+                if(i == 0 && j == 0){
+                    if(grid[i+1][j] == 'X') temp++;
+                    if(grid[i][j+1] == 'X') temp++;
+                    if(grid[i+1][j+1] == 'X') temp++;
+                    if(temp > 48) grid[i][j] = (char)temp;
+                }
+
+                if(i == 0 && j == 8){
+                    if(grid[i][j-1] == 'X') temp++;
+                    if(grid[i+1][j-1] == 'X') temp++;
+                    if(grid[i+1][j] == 'X') temp++;
+                    if(temp > 48) grid[i][j] = (char)temp;
+                }
+
+                if(i == 8 && j == 0){
+                    if(grid[i-1][j] == 'X') temp++;
+                    if(grid[i-1][j+1] == 'X') temp++;
+                    if(grid[i][j+1] == 'X') temp++;
+                    if(temp > 48) grid[i][j] = (char)temp;
+                }
+
+                if(i == 8 && j == 8){
+                    if(grid[i][j-1] == 'X') temp++;
+                    if(grid[i-1][j-1] == 'X') temp++;
+                    if(grid[i-1][j] == 'X') temp++;
+                    if(temp > 48) grid[i][j] = (char)temp;
+                }
+
+                if(i == 0 && j != 0 && j != 8){
+                    if(grid[i][j-1] == 'X') temp++;
+                    if(grid[i+1][j-1] == 'X') temp++;
+                    if(grid[i+1][j] == 'X') temp++;
+                    if(grid[i+1][j+1] == 'X') temp++;
+                    if(grid[i][j+1] == 'X') temp++;
+                    if(temp > 48) grid[i][j] = (char)temp;
+                }
+
+                if(i == 8 && j != 0 && j != 8){
+                    if(grid[i][j-1] == 'X') temp++;
+                    if(grid[i-1][j-1] == 'X') temp++;
+                    if(grid[i-1][j] == 'X') temp++;
+                    if(grid[i-1][j+1] == 'X') temp++;
+                    if(grid[i][j+1] == 'X') temp++;
+                    if(temp > 48) grid[i][j] = (char)temp;
+                }
+
+                if(i != 0 && i != 8 && j == 0){
+                    if(grid[i-1][j] == 'X') temp++;
+                    if(grid[i-1][j+1] == 'X') temp++;
+                    if(grid[i][j+1] == 'X') temp++;
+                    if(grid[i+1][j+1] == 'X') temp++;
+                    if(grid[i+1][j] == 'X') temp++;
+                    if(temp > 48) grid[i][j] = (char)temp;
+                }
+
+                if(i != 0 && i != 8 && j == 8){
+                    if(grid[i-1][j] == 'X') temp++;
+                    if(grid[i-1][j-1] == 'X') temp++;
+                    if(grid[i][j-1] == 'X') temp++;
+                    if(grid[i+1][j-1] == 'X') temp++;
+                    if(grid[i+1][j] == 'X') temp++;
+                    if(temp > 48) grid[i][j] = (char)temp;
+                }
+
+                if(i>0 && j>0 && i<8 && j<8){
+                    if(grid[i-1][j-1] == 'X') temp++;
+                    if(grid[i][j-1] == 'X') temp++;
+                    if(grid[i+1][j-1] == 'X') temp++;
+                    if(grid[i-1][j] == 'X') temp++;
+                    if(grid[i+1][j] == 'X') temp++;
+                    if(grid[i-1][j+1] == 'X') temp++;
+                    if(grid[i][j+1] == 'X') temp++;
+                    if(grid[i+1][j+1] == 'X') temp++;
+                    if(temp > 48) grid[i][j] = (char)temp;
+                }
+
                 }
             }
         }
